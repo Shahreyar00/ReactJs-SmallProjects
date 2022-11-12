ced1142198c0c880e49b0efc775a95cf9dbd77e6
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2022-11-12 12:13:23 +0530

    added ProgressBar

--- multipage-form/src/App.jsx
+++ multipage-form/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "./components/Form";
+import ProgressBar from "./components/ProgressBar";
 import { FormProvider } from "./context/FormContext";
 
 const App = () => {
     return (
         <FormProvider>
+            <ProgressBar />
             <Form />
         </FormProvider>
     )
